8a6934fa287091ede63b7e3c408af777d1cce54c
svanlaugbirna <svanlaugbirna@gmail.com>
2024-02-15 11:19:40 +0000

    finiseh the turorial

---
+++ mongodbtest/src/app/back/page.tsx
@@ -0,0 +1,4 @@
+export default function Back() {
+    console.log("Hello back end")
+    return (<h1>Some server side rendered content</h1>)
+}
---
+++ mongodbtest/src/app/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+export default function Both() {
+  console.log("hello both")
+  return (<h1>Some content</h1>)
+}
